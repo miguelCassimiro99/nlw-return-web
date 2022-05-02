d3aeea3959a7a0c405086e1e54d3b22015d659b7
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2022-05-02 20:21:44 -0300

    finished tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-  return <h1>Hello world - React App</h1>
+  return <h1 className="
+  flex justify-center
+  p-4
+  bg-violet-600
+  text-white
+  hover:bg-violet-100
+  hover:text-black
+  focus:bg-red-400
+  ">Hello world - React App</h1>
 }
 
 export default App
